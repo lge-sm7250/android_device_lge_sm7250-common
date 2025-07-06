b289f3682f3ac8ff4d2921d15c262e2a29cfc620
Han Sol Jin <hansol@hansol.ca>
2025-07-06 03:45:36 -0700

    [FIXME] sm7250-common: don't build camera service
    - Camera needs Binder threadpool patch
    
    Change-Id: I3f201ebbb0edc0ef7c815b5be71cb9633c50e7e3

--- sm7250-common.mk
+++ sm7250-common.mk
@@ -82,9 +82,9 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-service
 
 # Camera
-PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
+#PRODUCT_PACKAGES += \
+#    android.hardware.camera.provider@2.4-impl \
+#    android.hardware.camera.provider@2.4-service_64
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
